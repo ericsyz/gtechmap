c4588f7bac230db06d217c9260535982b3c765ce
justinhuang113 <68916736+justinhuang113@users.noreply.github.com>
2021-10-23 14:45:39 -0400

    Add files via upload

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -1,60 +1,55 @@
 //
 //  ContentView.swift
-//  Shared
+//  ToDoApp
 //
-//  Created by Eric Zhou on 10/23/21.
+//  Created by Jared on 4/6/20.
+//  Copyright © 2020 Davidson Family. All rights reserved.
 //
 
 import SwiftUI
-import MapKit
+import Combine
 
 struct ContentView: View {
+    @ObservedObject var taskStore = TaskStore()
+    @State var newToDo : String = ""
     
-    @State private var centerCoordinate = CLLocationCoordinate2D()
-    @State private var locations = [MKPointAnnotation]()
-    @State private var selectedPlace: MKPointAnnotation?
-    @State private var showingPlaceDetails = false
-    @State private var showingEditScreen = false
+    var searchBar : some View {
+        HStack {
+            TextField("Enter in a new task", text: self.$newToDo)
+            Button(action: self.addNewToDo, label: {
+                Text("Add New")
+            })
+        }
+    }
+    
+    func addNewToDo() {
+        taskStore.tasks.append(Task(id: String(taskStore.tasks.count + 1), toDoItem: newToDo))
+        self.newToDo = ""
+        //Add auto generated id in the future.
+    }
     
     var body: some View {
-        ZStack {
-            MapView(annotations: locations, centerCoordinate:$centerCoordinate, selectedPlace: $selectedPlace, showingPlaceDetails: $showingPlaceDetails).edgesIgnoringSafeArea(.all)
-            Circle().fill(Color.blue).opacity(0.3).frame(width:32, height:32)
+        NavigationView {
             VStack {
-                Spacer()
-                HStack {
-                    Spacer()
-                    Button(action: {
-                        let newLocation = MKPointAnnotation()
-                        newLocation.title = "Example"
-                        newLocation.coordinate = self.centerCoordinate
-                        self.locations.append(newLocation)
-                        self.selectedPlace = newLocation
-                        self.showingEditScreen = true
-                    }) {
-                        Image(systemName: "plus")
-                    }
-                    .padding()
-                    .background(Color.black.opacity(0.75))
-                    .foregroundColor(.white)
-                    .font(.title)
-                    .clipShape(Circle())
-                    .padding(.trailing)
-                    
-                }
-            }
-            .alert(isPresented: $showingPlaceDetails) {
-                Alert(title: Text(selectedPlace?.title ?? "Unknown"), message: Text(selectedPlace?.subtitle ?? "Missing Place Info"), primaryButton: .default(Text("OK")), secondaryButton: .default(Text("Edit")) {
-                    self.showingEditScreen = true
-                })
-            }
-        }
-        .sheet(isPresented: $showingEditScreen) {
-            if self.selectedPlace != nil {
-                EditView(placemark: self.selectedPlace!)
+                searchBar.padding()
+                List {
+                    ForEach(self.taskStore.tasks) { task in
+                        Text(task.toDoItem)
+                    }.onMove(perform: self.move)
+                        .onDelete(perform: self.delete)
+                }.navigationBarTitle("Tasks")
+                .navigationBarItems(trailing: EditButton())
             }
         }
     }
+    func move(from source : IndexSet, to destination : Int) {
+        taskStore.tasks.move(fromOffsets: source, toOffset: destination)
+    }
+    
+    func delete(at offsets : IndexSet) {
+        taskStore.tasks.remove(atOffsets: offsets)
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -62,4 +57,3 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-
