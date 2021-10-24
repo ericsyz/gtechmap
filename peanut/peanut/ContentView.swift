88675f5ea83317cfc018c46af46afe576edde9a2
ericsyz <52880789+ericsyz@users.noreply.github.com>
2021-10-24 02:46:58 -0400

    Add files via upload

---
+++ peanut/peanut/ContentView.swift
@@ -0,0 +1,63 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Eric Zhou on 10/23/21.
+//
+
+import SwiftUI
+import MapKit
+
+struct ContentView: View {
+    
+    @State private var centerCoordinate = CLLocationCoordinate2D()
+    @State public var locations = [MKPointAnnotation]()
+    @State private var selectedPlace: MKPointAnnotation?
+    @State private var showingPlaceDetails = false
+    @State private var showingEditScreen = false
+    var body: some View {
+        ZStack {
+            MapView(centerCoordinate:$centerCoordinate, selectedPlace: $selectedPlace, showingPlaceDetails: $showingPlaceDetails, annotations: locations).edgesIgnoringSafeArea(.all)
+            Circle().fill(Color.blue).opacity(0.3).frame(width:32, height:32)
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        let newLocation = MKPointAnnotation()
+                        newLocation.title = "Example"
+                        newLocation.coordinate = self.centerCoordinate
+                        self.locations.append(newLocation)
+                        self.selectedPlace = newLocation
+                        self.showingEditScreen = true
+                    }) {
+                        Image(systemName: "plus")
+                    }
+                    .padding()
+                    .background(Color.black.opacity(0.75))
+                    .foregroundColor(.white)
+                    .font(.title)
+                    .clipShape(Circle())
+                    .padding(.trailing)
+                    
+                }
+            }
+        }
+        .alert(isPresented: $showingPlaceDetails) {
+            Alert(title: Text(selectedPlace?.title ?? "Unknown"), message: Text(selectedPlace?.subtitle ?? "Missing Place Info"), primaryButton: .default(Text("OK")), secondaryButton: .default(Text("Edit")) {
+                self.showingEditScreen = true
+            })
+        }
+        .sheet(isPresented: $showingEditScreen) {
+            if self.selectedPlace != nil {
+                EditView(placemark: self.selectedPlace!)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ peanut/peanut/peanutApp.swift
@@ -0,0 +1,19 @@
+
+//  testApp.swift
+//  Shared
+//
+//  Created by Eric Zhou on 10/23/21.
+//
+
+import SwiftUI
+import MapKit
+import Foundation
+
+@main
+struct peanutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavView()
+        }
+    }
+}
